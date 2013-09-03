94d564b43de72be3e4c2e262bb4182e154561c43
John F <ejohn@feigdev.com>
2013-09-02 20:59:22 -0700

    add callback

--- src/main/java/com/feigdev/androidserialsql/WriterTask.java
+++ src/main/java/com/feigdev/androidserialsql/WriterTask.java
@@ -6,10 +6,12 @@ import android.database.sqlite.SQLiteDatabase;
  * Created by ejohn on 9/2/13.
  */
 public abstract class WriterTask implements Runnable {
+    public Runnable callback;
     private SQLiteDatabase db;
 
-    public WriterTask(String dbName) {
+    public WriterTask(String dbName, Runnable callback) {
         this.db = AccessDB.databases.get(dbName).getWriter();
+        this.callback = callback;
     }
 
     public SQLiteDatabase getDB() {
