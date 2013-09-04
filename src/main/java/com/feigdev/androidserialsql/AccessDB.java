f953881df56bd3ed6ef35ec630c07999ca6e9ace
John F <ejohn@feigdev.com>
2013-09-03 19:53:59 -0700

    made it more static

--- src/main/java/com/feigdev/androidserialsql/AccessDB.java
+++ src/main/java/com/feigdev/androidserialsql/AccessDB.java
@@ -14,29 +14,22 @@ import java.util.concurrent.TimeUnit;
  */
 public class AccessDB {
     private static final LinkedBlockingQueue<Runnable> writerQueue = new LinkedBlockingQueue<Runnable>();
-    private final GenericDBHelper dbHelper;
-    private final String dbName;
+    protected static final ConcurrentHashMap<String, DBPair> databases = new ConcurrentHashMap<String, DBPair>();
+    private static final ExecutorService executorService = new ThreadPoolExecutor(1, 1, 30, TimeUnit.SECONDS, writerQueue, new ThreadPoolExecutor.CallerRunsPolicy());
 
-    static final ConcurrentHashMap<String, DBPair> databases = new ConcurrentHashMap<String, DBPair>();
-    private ExecutorService executorService = new ThreadPoolExecutor(1, 1, 30, TimeUnit.SECONDS, writerQueue, new ThreadPoolExecutor.CallerRunsPolicy());
-
-    public AccessDB(Context context, DefineDB myDB) {
-        dbHelper = new GenericDBHelper(context, myDB);
-        dbName = myDB.getDbName();
+    public static void addDB(Context context, DefineDB myDB) {
+        GenericDBHelper dbHelper = new GenericDBHelper(context, myDB);
+        String dbName = myDB.getDbName();
         SQLiteDatabase dbWrite = dbHelper.getWritableDatabase();
         SQLiteDatabase dbRead = dbHelper.getReadableDatabase();
         databases.put(dbName, new DBPair(dbRead, dbWrite));
-    }
-
-    public SQLiteDatabase getReadableDB() {
-        return databases.get(dbName).getReader();
     }
 
     public static SQLiteDatabase getReadableDB(String dbName){
         return databases.get(dbName).getReader();
     }
 
-    public void addWriteTask(WriterTask task) {
+    public static void addWriteTask(WriterTask task) {
         executorService.execute(task);
     }
 
--- src/main/java/com/feigdev/androidserialsql/WriterTask.java
+++ src/main/java/com/feigdev/androidserialsql/WriterTask.java
@@ -6,15 +6,11 @@ import android.database.sqlite.SQLiteDatabase;
  * Created by ejohn on 9/2/13.
  */
 public abstract class WriterTask implements Runnable {
+    public SQLiteDatabase db;
     public Runnable callback;
-    private SQLiteDatabase db;
 
     public WriterTask(String dbName, Runnable callback) {
         this.db = AccessDB.databases.get(dbName).getWriter();
         this.callback = callback;
-    }
-
-    public SQLiteDatabase getDB() {
-        return this.db;
     }
 }
